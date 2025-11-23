ccef7731eca54e7236f6e18efd0c1ea3bc62b039
unknown <davivarejao@gmail.com>
2025-11-23 19:01:43 -0300

    fix: correções mínimas na sintaxe da ./core

--- core/asgi.py
+++ core/asgi.py
@@ -1,10 +1,11 @@
 import os
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import toaki_app.websocket_urls
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
