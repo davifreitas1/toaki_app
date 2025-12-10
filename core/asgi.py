f735cfcefe2c92d9e6538bdfdffd1ce432a72faf
unknown <DomRaposo>
2025-12-10 11:59:44 -0300

    feat: testes deploy2

--- core/asgi.py
+++ core/asgi.py
@@ -1,17 +1,20 @@
 import os
 from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import toaki_app.websocket_urls
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+django_asgi_app = get_asgi_application()
+
+
+
+
+from toaki_app.websocket_urls import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter(
-            toaki_app.websocket_urls.websocket_urlpatterns
+        URLRouter(websocket_urlpatterns)
         )
-    ),
+    ,
 })
