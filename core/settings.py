2c8674d5eb2e436b15b3332593b7f0b7b00718da
Felipe Oliveira <164811788+donnraposo@users.noreply.github.com>
2025-12-15 13:50:47 -0300

    fix: adicionando link novo DNS

--- core/settings.py
+++ core/settings.py
@@ -64,11 +64,13 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5173",
+    "https://toaki.app.br",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://toaki.app.br",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
